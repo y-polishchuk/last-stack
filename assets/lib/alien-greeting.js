1227a51268bde1598f2b814946a8f478c287b5c3
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-16 19:10:33 +0200

    add css

--- assets/lib/alien-greeting.js
+++ assets/lib/alien-greeting.js
@@ -1,3 +1,8 @@
 export default function(message, inPeace = false) {
+  if(!inPeace) {
+    setTimeout(() => {
+        import('../styles/alien-greeting.css');
+    }, 4000)
+  }
   console.log(`${message}! ${inPeace ? '👽' : '👾'}`);
 }
