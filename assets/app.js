0f8b2c1250e1c182b4a7a15aea3ec49dfe917f09
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-16 17:45:10 +0200

    javascript modules

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,8 @@
  */
 import './styles/app.css';
 
+import alienGreeting from './lib/alien-greeting.js'; // in webpack we don't need the extension, but in real JS we need
+
+alienGreeting('Give us all your candy!', false);
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
