09fdf1cc4e45e0fe772d588f39f2034f479c3cf6
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-18 16:18:33 +0200

    add symfony/ux-turbo - turbo drive

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
+import * as Turbo from '@hotwired/turbo';
 import './bootstrap.js';
 import './styles/app.css';
 import alienGreeting from './lib/alien-greeting.js'; // in webpack we don't need the extension, but in real JS we need
 
+//Turbo.session.drive = false;
 alienGreeting('Give us all your candy!', false);
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
