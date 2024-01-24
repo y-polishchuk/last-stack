87ab78ef2ea307fd0272929936e7997e9af950e0
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-24 18:44:41 +0200

    add pagination & column sorting

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use Pagerfanta\Pagerfanta;
 use App\Repository\PlanetRepository;
 use App\Repository\VoyageRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -16,16 +18,28 @@ class MainController extends AbstractController
     public function homepage(
         VoyageRepository $voyageRepository,
         PlanetRepository $planetRepository,
-        #[MapQueryParameter('query')] string $query = null,
+        #[MapQueryParameter] int $page = 1,
+        #[MapQueryParameter] string $sort = 'leaveAt',
+        #[MapQueryParameter] string $sortDirection = 'ASC',
+        #[MapQueryParameter] string $query = null,
         #[MapQueryParameter('planets', \FILTER_VALIDATE_INT)] array $searchPlanets = [],
     ): Response
     {
-        $voyages = $voyageRepository->findBySearch($query, $searchPlanets);
+        $validSorts = ['purpose', 'leaveAt'];
+        $sort = in_array($sort, $validSorts) ? $sort : 'leaveAt';
+
+        $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
+            new QueryAdapter($voyageRepository->findBySearchQueryBuilder($query, $searchPlanets, $sort, $sortDirection)),
+            $page,
+            10
+        );
 
         return $this->render('main/homepage.html.twig', [
-            'voyages' => $voyages,
+            'voyages' => $pager,
             'planets' => $planetRepository->findAll(),
             'searchPlanets' => $searchPlanets,
+            'sort' => $sort,
+            'sortDirection' => $sortDirection,
         ]);
     }
 }
