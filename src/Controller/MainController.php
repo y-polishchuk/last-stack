fe6bfdc880057554726cc2fee4169675b085d089
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-24 19:12:07 +0200

    add turbo frames to datatable

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -27,7 +27,7 @@ class MainController extends AbstractController
     {
         $validSorts = ['purpose', 'leaveAt'];
         $sort = in_array($sort, $validSorts) ? $sort : 'leaveAt';
-
+        
         $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
             new QueryAdapter($voyageRepository->findBySearchQueryBuilder($query, $searchPlanets, $sort, $sortDirection)),
             $page,
