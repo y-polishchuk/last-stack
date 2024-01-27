4dffa85d92f4b326de87519db88263d51c9fa038
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-27 12:40:00 +0200

    add ajax-load to the modal

--- assets/controllers/modal_controller.js
+++ assets/controllers/modal_controller.js
@@ -1,7 +1,37 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['dialog'];
+    static targets = ['dialog', 'dynamicContent'];
+
+    observer = null;
+
+    connect() {
+        if (this.hasDynamicContentTarget) {
+            // when the content changes, call this.open()
+            this.observer = new MutationObserver(() => {
+                const shouldOpen = this.dynamicContentTarget.innerHTML.trim().length > 0;
+                if (shouldOpen && !this.dialogTarget.open) {
+                    this.open();
+                } else if (!shouldOpen && this.dialogTarget.open) {
+                    this.close();
+                }
+            });
+            this.observer.observe(this.dynamicContentTarget, {
+                childList: true,
+                characterData: true,
+                subtree: true
+            });
+        }
+    }
+    
+    disconnect() {
+        if (this.observer) {
+            this.observer.disconnect();
+        }
+        if (this.dialogTarget.open) {
+            this.close();
+        }
+    }
 
     open() {
         this.dialogTarget.showModal();
