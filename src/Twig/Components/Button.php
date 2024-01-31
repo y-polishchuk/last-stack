a40bea73b9048f636cbad4c64a6921e2c11f4e7a
MandalorianFighter <yanush.polishchuk@gmail.com>
2024-01-31 16:51:12 +0200

    add modal twig component

--- src/Twig/Components/Button.php
+++ src/Twig/Components/Button.php
@@ -16,6 +16,7 @@ final class Button
             'default' => 'text-white bg-blue-500 hover:bg-blue-700',
             'success' => 'text-white bg-green-600 hover:bg-green-700',
             'danger' => 'text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:ring-red-300 focus:outline-none',
+            'link' => 'text-white',
             default => throw new \LogicException(sprintf('Unknown button type "%s"', $this->variant)),
         };
     }
